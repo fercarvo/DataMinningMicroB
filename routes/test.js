9556ef41a21e444479d45984bd77c957c894d9df
wjvelez <wjvelez@espol.edu.ec>
2017-11-26 23:01:17 -0500

    agregado gráfico de mapa de calor

--- routes/test.js
+++ routes/test.js
@@ -8,21 +8,22 @@ const { longCompute, processPromise, JPP } = require('../util/process.js')
 var nj = require('numjs')
 
 //Performance test multi hilo
-router.get("/test0", function(req, res, next) {
+router.get("/resultado", function(req, res, next) {
+	//var x = nj.array([[1,2,3], [4,5,6], [7,8,9]])
+	var x = matrizAleatoria(50, 30, 10, 100)
+	//var r = nj.array([[1,1,1], [2,2,2], [3,3,3]])
+	var r = matrizAleatoria(5, 30, 10, 100)
+	var k = 5
+	//var k = 3
+	var alpha = 10000000
+	var lambda = 0.05
+	var epsilon = 0.01
+	var maxiter = 100
+	var verbose = false
+	var resultado = JPP(x, r, k, alpha, lambda, epsilon, maxiter)
 
-	var begin = Date.now()
-
-	var proc = []
-
-	proc.push( processPromise(__dirname + '/../util/childProcess.js', " 1") )
-	proc.push( processPromise(__dirname + '/../util/childProcess.js', " 2") )
-	proc.push( processPromise(__dirname + '/../util/childProcess.js', " 3") )
-	proc.push( processPromise(__dirname + '/../util/childProcess.js', " 4") )
-
+	res.json(resultado.M.tolist())
 	
-	Promise.all(proc).then(function (){
-		res.send("Performance time: " + (Date.now() - begin)/1000 + "s")
-	})
 })
 
 
@@ -59,38 +60,3 @@ function matrizAleatoria(N , M, min, max) {
 	return nj.array(X)
 }
 
-//var x = nj.array([[1,2,3], [4,5,6], [7,8,9]])
-var x = matrizAleatoria(50, 30, 10, 100)
-//var r = nj.array([[1,1,1], [2,2,2], [3,3,3]])
-var r = matrizAleatoria(60, 30, 10, 100)
-var k = 60
-//var k = 3
-var alpha = 10000000
-var lambda = 0.05
-var epsilon = 0.01
-var maxiter = 100
-var verbose = false
-//console.log("shape", x.shape)
-/*
-console.log("x", x)
-console.log("r", r)
-console.log("k", k)
-console.log("alpha", alpha)
-console.log("lambda", lambda)
-console.log("epsilon", epsilon)
-console.log("maxiter", maxiter)
-console.log("verbose", verbose)
-*/
-console.time("tiempo")
-
-console.log("EL JPP es: ", JPP(x, r, k, alpha, lambda, epsilon, maxiter))
-
-console.timeEnd("tiempo")
-
-/*
-//console.log("prueba random", matriz)
-console.log('matrizA: ', matrizA)
-console.log('sumatoria: ', matrizA.sum())
-console.log('diagonal: ', matrizA.diag())
-console.log('suma de diagonal: ', matrizA.diag().sum())
-*/
